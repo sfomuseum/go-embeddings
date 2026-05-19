1b9d727b8e8eb7554f19f878aa962826156fb6a7
Aaron Straup Cope <thisisaaronland@users.noreply.github.com>
2026-05-19 11:22:53 -0700

    Add `route://` Embedder implementation (#14)
    * snapshot: start working on 'routing' client
    
    * logic for creating router clients
    
    * improved docs
    
    * docs
    
    ---------
    
    Co-authored-by: thisisaaronland <thisisaaronland@localhost>

--- app/embeddings/flags.go
+++ app/embeddings/flags.go
@@ -2,19 +2,32 @@ package embeddings
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	"github.com/sfomuseum/go-flags/flagset"
 )
 
 var client_uri string
 var precision int
+var model string
+var verbose bool
 
 func DefaultFlagSet() *flag.FlagSet {
 
 	fs := flagset.NewFlagSet("embeddings")
 
-	fs.StringVar(&client_uri, "client-uri", "null://", "...")
-	fs.IntVar(&precision, "precision", 32, "...")
+	fs.StringVar(&client_uri, "client-uri", "null://", "A registered sfomuseum/go-embeddings.Embedder[T] URI.")
+	fs.StringVar(&model, "model", "", "An optional model to specify when generating embeddings.")
+	fs.IntVar(&precision, "precision", 32, "The float-precision to use to for the embeddings that are returned.")
+	fs.BoolVar(&verbose, "verbose", false, "Enable verbose (debug) logging.")
+
+	fs.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Derive vector embeddings for a text string or image file.\n")
+		fmt.Fprintf(os.Stderr, "Usage:\n\t%s [options] [text|image] arg(N) arg(N)\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Valid options are:\n")
+		fs.PrintDefaults()
+	}
 
 	return fs
 }
---
+++ route_test.go
@@ -0,0 +1,76 @@
+package embeddings
+
+import (
+	"context"
+	"io"
+	"os"
+	"testing"
+)
+
+func TestRouteEmbeddings(t *testing.T) {
+
+	ctx := context.Background()
+
+	emb, err := NewEmbedder32(ctx, "route://?client-uri=null://...null/model")
+
+	if err != nil {
+		t.Fatalf("Failed to create embedder, %v", err)
+	}
+
+	req := &EmbeddingsRequest{
+		Body:  []byte("Hello world"),
+		Model: "null/model",
+	}
+
+	rsp, err := emb.TextEmbeddings(ctx, req)
+
+	if err != nil {
+		t.Fatalf("Failed to derive embeddings, %v", err)
+	}
+
+	if len(rsp.Embeddings()) != 0 {
+		t.Fatalf("Unexpected embedding")
+	}
+}
+
+func TestRouteImageEmbeddings(t *testing.T) {
+
+	ctx := context.Background()
+
+	emb, err := NewEmbedder64(ctx, "route://?client-uri=null://...null/model")
+
+	if err != nil {
+		t.Fatalf("Failed to create embedder, %v", err)
+	}
+
+	im_path := "fixtures/1527845303_walrus.jpg"
+
+	im_r, err := os.Open(im_path)
+
+	if err != nil {
+		t.Fatalf("Failed to open %s for reading, %v", im_path, err)
+	}
+
+	defer im_r.Close()
+
+	im_body, err := io.ReadAll(im_r)
+
+	if err != nil {
+		t.Fatalf("Failed to read data from %s, %v", im_path, err)
+	}
+
+	req := &EmbeddingsRequest{
+		Body:  im_body,
+		Model: "null/model",
+	}
+
+	rsp, err := emb.ImageEmbeddings(ctx, req)
+
+	if err != nil {
+		t.Fatalf("Failed to derive embeddings, %v", err)
+	}
+
+	if len(rsp.Embeddings()) != 0 {
+		t.Fatalf("Unexpected embedding")
+	}
+}
